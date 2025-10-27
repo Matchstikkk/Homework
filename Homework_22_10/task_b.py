06b32aac8768ebb60b7dbb7172eae653aeadd000
Matchstikkk <dymokkot64@gmail.com>
2025-10-27 21:34:27 +0300

    Update task_b.py

--- Homework_22_10/task_b.py
+++ Homework_22_10/task_b.py
@@ -1,11 +1,9 @@
-a, b, c = int(input()) # если все на одной строке
-'''
 a = int(input())
 b = int(input())
 c = int(input())
-если построчный ввод
-'''
+
 if a != b or b != c or a != c :
   print("True")
 else:
   print("False")
+  
